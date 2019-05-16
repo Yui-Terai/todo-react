412761838b876d896e011214433b19f12055b2e3
Yui Terai <yuiterai88@gmail.com>
2019-05-16 17:04:37 +0800

    Done deliverables

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-class List extends React.Component {
+class Itemlist extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -10,12 +10,11 @@ class List extends React.Component {
     this.removeHandler = this.removeHandler.bind(this);
   }
 
-
-  removeHandler=(index)=> {
-    let list = this.state.list;
-    list.splice(index, 1);
-    this.setState({ list: list });
+  changeHandler(event) {
+    this.setState({ word: event.target.value });
+    console.log("change", event.target.value);
   }
+
 
   clickHandler() {
     let newList = this.state.list;
@@ -25,43 +24,143 @@ class List extends React.Component {
     console.log("this is state list:" + this.state.list);
   }
 
-  changeHandler(event) {
-    this.setState({ word: event.target.value });
-    console.log("change", event.target.value);
-  }
+
+
+  removeHandler = index => {
+    let list = this.state.list;
+    list.splice(index, 1);
+    this.setState({ list: list });
+  };
+
 
   render() {
-    // render the list with a map() here
-    const listItems = this.state.list.map((item, index) => {
-      return <div key={index}>
-                <li>{item}</li>
-                <button name={index}
-                  onClick={() => this.removeHandler(index)
-                  }
-                >
-                  delete
-                </button>
-            </div>
-    });
+    return(
+      <div>
+        <Form changeHandler = { (event) => {this.changeHandler(event)} } clickHandler = { () => {this.clickHandler()} } word = {this.state.word} />
+        <TodoItem removeHandler = { (index) => {this.removeHandler(index)} } list = {this.state.list} />
+      </div>
+    )
+  }
+}
 
 
-    console.log("rendering");
-    return (
+
+
+
+
+class Form extends React.Component {
+  render() {
+    return(
       <div className="list">
-        <input onChange={this.changeHandler} value={this.state.word} />
+        <input onChange = { (event) => {this.props.changeHandler(event)} } value={this.props.word}/>
         <button
-          onClick={() => {
-            this.clickHandler();
+          onClick = {() => {  
+            this.props.clickHandler();
           }}
         >
           add Item
-        </button>
+        </button> 
+      </div>
+    )
+  }
+}
+
+
+
+
+
+
+
+
+
+class TodoItem extends React.Component {
+  render() {
+    const listItems = this.props.list.map((item, index) => {
+            return (
+              <div key={index}>
+                <li>{item}</li>
+                <button name={index} onClick={() => this.props.removeHandler(index)}>
+                  delete
+                </button>
+              </div>
+            );
+          });
+    return(
+      <div>
         <ul>
           {listItems}
         </ul>
       </div>
-    );
+    )
   }
 }
 
-ReactDOM.render(<List />, document.getElementById("root"));
+
+
+
+
+
+
+
+// class List extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       list: [],
+//       word: ""
+//     };
+//     this.changeHandler = this.changeHandler.bind(this);
+//     this.clickHandler = this.clickHandler.bind(this);
+//     this.removeHandler = this.removeHandler.bind(this);
+//   }
+
+//   removeHandler = index => {
+//     let list = this.state.list;
+//     list.splice(index, 1);
+//     this.setState({ list: list });
+//   };
+
+//   clickHandler() {
+//     let newList = this.state.list;
+//     newList.push(this.state.word);
+//     this.setState({ list: newList });
+
+//     console.log("this is state list:" + this.state.list);
+//   }
+
+//   changeHandler(event) {
+//     this.setState({ word: event.target.value });
+//     console.log("change", event.target.value);
+//   }
+
+//   render() {
+//     // render the list with a map() here
+//     const listItems = this.state.list.map((item, index) => {
+//       return (
+//         <div key={index}>
+//           <li>{item}</li>
+//           <button name={index} onClick={() => this.removeHandler(index)}>
+//             delete
+//           </button>
+//         </div>
+//       );
+//     });
+
+//     console.log("rendering");
+//     return (
+//       <div className="list">
+//         <input onChange={this.changeHandler} value={this.state.word} />
+//         <button
+//           onClick={() => {
+//             this.clickHandler();
+//           }}
+//         >
+//           add Item
+//         </button>
+//         <ul>{listItems}</ul>
+//       </div>
+//     );
+//   }
+// }
+
+ReactDOM.render(<Itemlist />, document.getElementById("root"));
